fe1e21f78e0367cb2617320da35af8c62245c42c
Medhi BUSTIN <medhi.bustin@etudiant.univ-lille1.fr>
2015-03-23 11:29:01 +0100

    REUSEADDR

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,6 +7,7 @@
 int creer_serveur(int port){
 
 	int socket_serveur;
+	int optval = 1;
 	
 	/*Création de la socket serveur*/
 	socket_serveur = socket(AF_INET ,SOCK_STREAM ,0);
@@ -20,6 +21,12 @@ int creer_serveur(int port){
 	saddr.sin_family = AF_INET; /* Socket ipv4 */
 	saddr.sin_port = htons(port); /* Port d ’ écoute */
 	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+
+	/*Permet à un processus de faire un bind sur une même adresse*/
+	if(setsockopt(socket_serveur , SOL_SOCKET , SO_REUSEADDR , &optval , sizeof(int)) == -1){
+		perror("setsockopt");
+		return -1;
+	}
 
 	/*Configuration de la socket*/
 	if (bind(socket_serveur, (struct sockaddr *)& saddr, sizeof(saddr)) == -1){	
